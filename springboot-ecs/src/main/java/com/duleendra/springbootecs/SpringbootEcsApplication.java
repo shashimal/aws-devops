372f2b77a34c440d1d828aba01d544e6e461a29e
shashimal <shashimald@gmail.com>
2020-08-28 02:12:16 +1200

    Created a springboot project

---
+++ springboot-ecs/src/main/java/com/duleendra/springbootecs/SpringbootEcsApplication.java
@@ -0,0 +1,13 @@
+package com.duleendra.springbootecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcsApplication.class, args);
+    }
+
+}
---
+++ springboot-ecs/src/test/java/com/duleendra/springbootecs/SpringbootEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duleendra.springbootecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
